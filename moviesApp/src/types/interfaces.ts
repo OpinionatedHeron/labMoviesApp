d83c08eb4da28f894abd3791e5b691bc6ff6d71b
OpinionatedHeron <leamoy@gmail.com>
2025-08-26 00:05:53 +0100

    Movie details page components
    Signed-off-by: OpinionatedHeron <leamoy@gmail.com>

--- moviesApp/src/types/interfaces.ts
+++ moviesApp/src/types/interfaces.ts
@@ -1,22 +1,28 @@
 export interface BaseMovieProps {
-    title: string;
-    budget: number;
-    homepage: string | undefined;
-    id: number;
-    imdb_id: string;
-    original_language: string;
-    overview: string;
-    release_date: string;
-    vote_average: number;
-    popularity: number;
-    poster_path?: string;
-    tagline: string;
-    runtime: number;
-    revenue: number;
-    vote_count: number;
-    favourite?: boolean;
-  }
- export interface BaseMovieListProps { 
-    movies: BaseMovieProps[];
-  }   
+  title: string;
+  budget: number;
+  homepage: string | undefined;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  popularity: number;
+  poster_path?: string;
+  tagline: string;
+  runtime: number;
+  revenue: number;
+  vote_count: number;
+  favourite?: boolean;
+}
+export interface BaseMovieListProps {
+  movies: BaseMovieProps[];
+}
 
+export interface MovieDetailsProps extends BaseMovieProps {
+  genres: {
+    id: number;
+    name: string;
+  }[];
+}
