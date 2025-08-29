8d68169ed801d01118a78d53c2b619a51b482d0b
Leanne Ahern <lahern@redhat.com>
2025-08-29 23:44:45 +0100

    Change site header
    Signed-off-by: Leanne Ahern <lahern@redhat.com>

--- moviesApp/src/index.tsx
+++ moviesApp/src/index.tsx
@@ -1,37 +1,29 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMovies";
 import MovieReviewPage from "./pages/movieReviewPage";
-
+import SiteHeader from "./components/siteHeader";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favourites">Favourites</Link>
-        </li>
-      </ul>
+      <SiteHeader /> {/* New Header  */}
       <Routes>
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+        <Route path="/reviews/:id" element={<MovieReviewPage />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
-
+  </React.StrictMode>
+);
