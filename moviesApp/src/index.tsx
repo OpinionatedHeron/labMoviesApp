f5ca3da0cdad1a1a87de79d56b971af2b7bb2328
Leanne Ahern <lahern@redhat.com>
2025-08-30 02:49:04 +0100

    Added Upcoming movies page.
    Signed-off-by: Leanne Ahern <lahern@redhat.com>

--- moviesApp/src/index.tsx
+++ moviesApp/src/index.tsx
@@ -6,6 +6,7 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMovies";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from "./components/siteHeader";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <SiteHeader /> {/* New Header  */}
       <Routes>
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
